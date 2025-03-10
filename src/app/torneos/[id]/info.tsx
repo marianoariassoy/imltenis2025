34af2435002f218c91be8cb2e0d64fe30ef9e731
marianoariassoy <hola@marianoarias.soy>
2025-03-10 18:46:31 -0300

    third commit

--- src/app/torneos/[id]/info.tsx
+++ src/app/torneos/[id]/info.tsx
@@ -2,8 +2,8 @@ import { Info } from "@/lib/icons";
 
 const info = ({ text }: { text: string }) => {
   return (
-    <div className="flex gap-x-2 items-center text-sm p-3 lg:px-6 bg-black/10 rounded-xl mb-3">
-      <span className="text-primary">
+    <div className="flex gap-x-2 text-sm p-3 bg-black/10 rounded-xl mb-3">
+      <span className="text-primary mt-1">
         <Info />
       </span>
       <span className="text-secondary whitespace-break-spaces">{text}</span>
