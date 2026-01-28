60cea8fb5e43720270e7a1d586181a76f64b6671
marianoariassoy <hola@marianoarias.soy>
2026-01-28 16:04:51 -0300

    third commit

--- src/app/soon/page.tsx
+++ src/app/soon/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
   return (
     <div className="absolute left-0 top-1/2 -translate-y-1/2 w-full fade-in flex flex-col items-center justify-center">
       <h1 className="text-3xl mb-1">🚀</h1>
-      <span className="text-primary text-center text-lg font-semibold">
+      <span className="text-primary text-center font-semibold">
         ¡Muy pronto!
       </span>
     </div>
