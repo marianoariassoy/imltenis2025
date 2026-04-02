5e64f237dca6e76c4ebae662fadb57867ad9950d
marianoariassoy <hola@marianoarias.soy>
2026-04-02 16:40:56 -0300

    third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import Footer from "@/components/footer";
 import AnalyticsProvider from "@/components/AnalyticsProvider";
 import { MenuProvider } from "@/context/menu-context";
