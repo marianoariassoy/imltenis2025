eca5320f764c8df3f6da9752ba4f0a242c2e094f
marianoariassoy <hola@marianoarias.soy>
2026-09-24 18:04:51 -0300

    feat: add build command

--- src/app/orden-de-juego/page.tsx
+++ src/app/orden-de-juego/page.tsx
@@ -13,9 +13,6 @@ export const metadata = {
 const page = async () => {
   const response = await fetch(
     process.env.NEXT_PUBLIC_API_URL + "/series/upcoming",
-    {
-      next: { revalidate: 600 },
-    },
   );
   const data = (await response.json()) as Serie[];
   if (!data)
