0f7cbbdc978c5309701a9f4196beb1be2349a6e0
marianoariassoy <hola@marianoarias.soy>
2026-09-24 18:24:24 -0300

    feat: add build command

--- src/app/rankings/campeones/page.tsx
+++ src/app/rankings/campeones/page.tsx
@@ -37,6 +37,9 @@ interface data {
 const page = async () => {
   const response = await fetch(
     process.env.NEXT_PUBLIC_API_URL + "/rankings/champions",
+    {
+      cache: "no-store",
+    },
   );
   const data = (await response.json()) as data[];
 
